f61aacabbb2fe37f0d3d977a5eb6f86a342197c9
theleal <89053362+theleal@users.noreply.github.com>
2025-11-08 10:47:54 -0300

    feat: created icons.

--- src/icons/source/index.ts
+++ src/icons/source/index.ts
@@ -1,3 +1,17 @@
+import Conservant from "./Conservant";
+import Drink from "./Drink";
+import Like from "./Like";
+import Plant from "./Plant";
+import Signal from "./Signal";
+import User from "./Stars";
+import Stars from "./Stars";
+
 export default {
-    
-}
+    plant: Plant,
+    conservant: Conservant,
+    drink: Drink,
+    like: Like,
+    signal: Signal,
+    stars: Stars,
+    user: User,
+};
